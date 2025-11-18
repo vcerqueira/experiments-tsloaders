1cd4f65405ea15e4078033e8278d0ce8d43af087
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-11-18 07:46:30 +0000

    example

--- example.py
+++ example.py
@@ -2,12 +2,16 @@ from pprint import pprint
 
 from loaders import ChronosDataset
 
-group = 'm4_daily'
+group = 'm5'
 
 # the chronos train split actually also contains the test set (last 18 observations)
 # ... at least for m4 monthly (didn't check others)
 df, *_ = ChronosDataset.load_everything(group=group)
 # df = ChronosDataset.load_data(group=group)
+
+# import datasets
+# ds = datasets.load_dataset(path='autogluon/chronos_datasets', name=group, split='train')
+# ds.set_format("numpy")
 
 # ----- data set list
 
